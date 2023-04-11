47fde6b97ae8630285506e8492187ae303fc3c9e
unknown <gabriel.carioca66@gmail.com>
2023-04-10 21:24:24 -0300

    Alteração no app

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ pyautogui.PAUSE = 1
 pyautogui.alert('O código vai começar. Não use o computador até a mensagem final.')
 
 produtos = []
-produtos2 = []
+
 
 with open('produtos.txt','r') as arquivo:
     for linha in arquivo:
@@ -14,10 +14,6 @@ with open('produtos.txt','r') as arquivo:
         if colunas[0].strip():
             produtos.append(colunas[0].strip())
 
-    for linha in arquivo:
-        colunas = linha.split(' ')
-        if len(colunas) == 1 and colunas[1].strip():
-            produtos2.append(colunas[1].strip())
 
 
 
@@ -28,10 +24,7 @@ with open('produtos.txt','r') as arquivo:
         pyautogui.press('enter')
 
 
-    for produto in produtos2:
-        pyautogui.click(1333,712, clicks = 2)
-        pyautogui.write(produto)
-        pyautogui.press('tab')
+time.sleep(2)
 
 
 
